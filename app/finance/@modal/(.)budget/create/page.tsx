5220cc330b7af8af285a1ce32badecbe5c707727
Richard Addo <ramankwah03@gmail.com>
2025-05-15 06:44:07 +0200

    app page refactored

--- app/finance/@modal/(.)budget/create/page.tsx
+++
@@ -1,5 +0,0 @@
-import Budget_Form from "@/components/forms/budget_form";
-
-export default function Create_Budget() {
-  return <Budget_Form />;
-}
--- app/finance/@modal/(.)pots/create/page.tsx
+++
@@ -1,5 +0,0 @@
-import Pot_Form from "@/components/forms/pot_form";
-
-export default function Create_Pot_Modal() {
-  return <Pot_Form />;
-}
